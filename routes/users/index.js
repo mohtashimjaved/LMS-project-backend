2271a0401918279e8036603cc6449f4333108ace
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-04-02 11:40:50 +0500

    updated

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,10 +3,13 @@ import getUser from "./get.js";
 import createUser from "./post.js";
 import loginUser from "./login.js";
 import tokenVerification from "../../config/tokenVerification.js";
+import refresh from "./refresh.js";
 
 const userRouter = express.Router()
 userRouter.get("/", tokenVerification, getUser);
 userRouter.post("/create", createUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/refresh", refresh);
+
 
 export default userRouter;
