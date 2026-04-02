d2d16113c414433df7a2ff08de57d3e3edeb478d
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-04-02 12:40:07 +0500

    Update index.js

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import getUser from "./get.js";
-import createUser from "./signup.js";
 import loginUser from "./login.js";
 import tokenVerification from "../../config/tokenVerification.js";
 import refresh from "./refresh.js";
